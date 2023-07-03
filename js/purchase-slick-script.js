3b3c34edf1578a2f1abf4cfd0d692808e73b7400
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-07-03 11:40:41 +0300

    Added purchase section

---
+++ js/purchase-slick-script.js
@@ -0,0 +1,11 @@
+jQuery(function () {
+    $('.purchase__image-wrapper').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        mobileFirst: true,
+        speed: 1000,
+    });
+});
